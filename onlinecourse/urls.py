4a93edcdf4f8b42f536fd7f0a4c8febbc9919f03
sepho0 <rosekainasephoraaltidor7@gmail.com>
2026-01-07 12:57:26 -0500

    Update URL patterns for course views

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -4,21 +4,26 @@ from django.conf.urls.static import static
 from . import views
 
 app_name = 'onlinecourse'
+
 urlpatterns = [
-    # route is a string contains a URL pattern
-    # view refers to the view function
-    # name the URL
-    path(route='', view=views.CourseListView.as_view(), name='index'),
+    # Liste des cours
+    path('', views.CourseListView.as_view(), name='index'),
+
+    # Authentification
     path('registration/', views.registration_request, name='registration'),
     path('login/', views.login_request, name='login'),
     path('logout/', views.logout_request, name='logout'),
-    # ex: /onlinecourse/5/
+
+    # Détail d’un cours
     path('<int:pk>/', views.CourseDetailView.as_view(), name='course_details'),
-    # ex: /enroll/5/
+
+    # Inscription à un cours
     path('<int:course_id>/enroll/', views.enroll, name='enroll'),
 
-    # <HINT> Create a route for submit view
+    # Soumission de l’examen
+    path('<int:course_id>/submit/', views.submit, name='submit'),
 
-    # <HINT> Create a route for show_exam_result view
+    # Affichage des résultats de l’examen
+    path('course/<int:course_id>/submission/<int:submission_id>/result/', views.show_exam_result, name='show_exam_result'),
 
- ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
